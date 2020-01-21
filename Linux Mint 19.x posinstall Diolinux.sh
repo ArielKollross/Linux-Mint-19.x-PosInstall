ef553445fbd2e69b0b1cb9189437317b0d63498d
Ariel Kollross <arielkollross@gmail.com>
2020-01-21 13:04:29 -0300

    Update Linux Mint 19.x posinstall Diolinux.sh

--- Linux Mint 19.x posinstall Diolinux.sh
+++ Linux Mint 19.x posinstall Diolinux.sh
@@ -7,20 +7,14 @@ PPA_GRAPHICS_DRIVERS="ppa:graphics-drivers/ppa"
 URL_WINE_KEY="https://dl.winehq.org/wine-builds/winehq.key"
 URL_PPA_WINE="https://dl.winehq.org/wine-builds/ubuntu/"
 URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-URL_SIMPLE_NOTE="https://github.com/Automattic/simplenote-electron/releases/download/v1.8.0/Simplenote-linux-1.8.0-amd64.deb"
-URL_4K_VIDEO_DOWNLOADER="https://dl.4kdownload.com/app/4kvideodownloader_4.9.2-1_amd64.deb"
-URL_INSYNC="https://d2t3ff60b2tol4.cloudfront.net/builds/insync_3.0.20.40428-bionic_amd64.deb"
 
 DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
 
 PROGRAMAS_PARA_INSTALAR=(
-  snapd
+  whatsapp-desktop 
   mint-meta-codecs
-  winff
   guvcview
-  virtualbox
   flameshot
-  nemo-dropbox
   steam-installer
   steam-devices
   steam:i386
@@ -38,6 +32,14 @@ PROGRAMAS_PARA_INSTALAR=(
   libfreetype6:i386
   libdbus-1-3:i386
   libsqlite3-0:i386
+)
+
+PROGRAMAS_PARA_DESINSTALAR=(
+  thuenderbrid
+  bluetooh
+  hexchat
+  libreoffice-math
+  libreoffice-draw
 )
 # ---------------------------------------------------------------------- #
 
@@ -68,9 +70,6 @@ sudo apt update -y
 ## Download e instalaçao de programas externos ##
 mkdir "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_GOOGLE_CHROME"       -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_SIMPLE_NOTE"         -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_4K_VIDEO_DOWNLOADER" -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_INSYNC"              -P "$DIRETORIO_DOWNLOADS"
 
 ## Instalando pacotes .deb baixados na sessão anterior ##
 sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
@@ -84,16 +83,20 @@ for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
   fi
 done
 
+for nome_do_programa in ${PROGRAMAS_PARA_DESINSTALAR[@]}; do
+  if ! dpkg -l | grep -q $nome_do_programa; then # Só instala se já não estiver instalado
+    apt remove "$nome_do_programa" -y
+  else
+    echo "[DESISTALADO] - $nome_do_programa"
+  fi
+done
+
+
 sudo apt install --install-recommends winehq-stable wine-stable wine-stable-i386 wine-stable-amd64 -y
 
 ## Instalando pacotes Flatpak ##
-flatpak install flathub com.obsproject.Studio -y
+flatpak install flathub com.spotify.Client -y
 
-## Instalando pacotes Snap ##
-sudo snap install spotify
-sudo snap install slack --classic
-sudo snap install skype --classic
-sudo snap install photogimp
 # ---------------------------------------------------------------------- #
 
 # ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
